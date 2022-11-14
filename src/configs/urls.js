f912006f8bb5a5c659a6186e84dd37602365a80c
KyryloPikhno <Mr.Good@i.ua>
2022-11-14 17:24:43 +0200

    added services

---
+++ src/configs/urls.js
@@ -0,0 +1,7 @@
+const baseURl = 'https://api.themoviedb.org/3';
+
+const urls = {
+    movies: '/discover/movie'
+};
+
+export {baseURl, urls};
---
+++ src/services/axios.service.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+import {baseURl} from "../configs";
+
+
+const _accessTokenKey = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkMDZmNTMyOGVhYjMwN2I3MTM5MjNmZTUzYzU5MjU2OSIsInN1YiI6IjYzNGFiZTM0MTA4OWJhMDA3ZDUzODY4MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ACz4f6I94aAvceySLFzh9mH4Asy4rutGDbrOl1qbDtE';
+
+const axiosService = axios.create({
+    baseUrl: baseURl
+});
+
+axiosService.interceptors.request.use((config) => {
+    config.headers.Authorization = `Bearer ${_accessTokenKey}`
+    return config
+});
+
+export {axiosService};
