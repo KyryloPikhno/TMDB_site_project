1558dd7580a7738c26d32ea07ae478484faf9d9c
KyryloPikhno <Mr.Good@i.ua>
2022-11-24 22:50:24 +0200

    added store

--- src/layouts/MainLayoutPage/MainLayoutPage.js
+++ src/layouts/MainLayoutPage/MainLayoutPage.js
@@ -1,9 +1,16 @@
+import {Outlet} from "react-router-dom";
+
+import {Header} from "../../components/Header/Header";
+import {Footer} from "../../components";
+
 const MainLayoutPage = () => {
 
 
     return (
         <div>
-            MainLayoutPage
+           <Header/>
+            <Outlet/>
+            <Footer/>
         </div>
     );
 };
