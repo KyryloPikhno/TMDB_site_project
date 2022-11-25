13df156c8077f75d8d446236d88ed59deaa07323
KyryloPikhno <Mr.Good@i.ua>
2022-11-25 20:07:36 +0200

    added routes

--- src/redux/slices/movie.slice.js
+++ src/redux/slices/movie.slice.js
@@ -1,4 +1,5 @@
-import {createSlice} from "@reduxjs/toolkit";
+import {createAsyncThunk, createSlice} from "@reduxjs/toolkit";
+import {movieService} from "../../services";
 
 
 const initialState = {
@@ -7,16 +8,41 @@ const initialState = {
     error:null
 };
 
+
+const getAll = createAsyncThunk(
+    'movieSlice/getAll',
+    async (_,{rejectWithValue})=>{
+        try {
+            const{data} = await  movieService.getAll()
+            return data
+        }catch (e){
+            return rejectWithValue(e.response.data)
+        }
+    }
+)
+
 const movieSlice= createSlice({
     name:'movieSlice',
     initialState,
-    reducers:{}
+    reducers:{},
+    extraReducers: builder =>
+        builder
+            .addCase(getAll.fulfilled, (state, action) => {
+                state.movies = action.payload
+                state.error = null
+                state.loading = false
+            })
+            .addCase(getAll.rejected, (state, action) => {
+                state.error = action.payload
+                state.loading = false
+            })
+
 })
 
 const {reducer: movieReducer, actions: {},} = movieSlice;
 
 const movieActions ={
-
+    getAll
 }
 
 export {movieReducer, movieActions, movieSlice};
