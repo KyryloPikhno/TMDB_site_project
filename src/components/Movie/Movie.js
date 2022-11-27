6c56900cb1b8dcd10d650b2e6e210dd84bf4e7b4
KyryloPikhno <Mr.Good@i.ua>
2022-11-27 17:28:21 +0200

    added movie

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -1,9 +1,17 @@
 const Movie = ({movie}) => {
 
+    const{id, title, vote_average, poster_path, genre_ids} = movie
+
+    const image_path = 'https://image.tmdb.org/t/p/w500'
 
     return (
         <div>
-            {movie.id}
+        <div>
+            {poster_path && <img src={`${image_path}${poster_path}`} alt={title}/>}
+        </div>
+    <div>
+        {title}
+    </div>
         </div>
     );
 };
