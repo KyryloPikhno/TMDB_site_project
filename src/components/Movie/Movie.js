7b215ea8f4f7b49ca38d70e42efec8edc1cfb55d
KyryloPikhno <Mr.Good@i.ua>
2022-11-27 17:42:21 +0200

    added css

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -1,15 +1,19 @@
+import css from './Movie.module.css'
+
 const Movie = ({movie}) => {
 
     const{id, title, vote_average, poster_path, genre_ids} = movie
 
     const image_path = 'https://image.tmdb.org/t/p/w500'
 
+    const currentTheme = undefined
+
     return (
+        <div className={currentTheme === 'dark'? css.card:css.lightCard}>
         <div>
-        <div>
-            {poster_path && <img src={`${image_path}${poster_path}`} alt={title}/>}
+            {poster_path && <img className={css.img} src={`${image_path}${poster_path}`} alt={title}/>}
         </div>
-    <div>
+    <div className={css.title}>
         {title}
     </div>
         </div>
