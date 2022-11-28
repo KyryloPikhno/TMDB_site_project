96407245143c9cac33822a19ff6e05ce2d40c30a
KyryloPikhno <Mr.Good@i.ua>
2022-11-28 14:28:16 +0200

    added genre

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -1,12 +1,23 @@
 import css from './Movie.module.css'
+import {useEffect, useState} from "react";
+import {useDispatch} from "react-redux";
+import {genreAction} from "../../services";
 
 const Movie = ({movie}) => {
+
+    const [genres, setGenres] = useState([])
 
     const {id, title, vote_average, poster_path, genre_ids} = movie
 
     const image_path = 'https://image.tmdb.org/t/p/w500'
 
     const currentTheme = undefined
+
+    const dispatch =useDispatch()
+
+    useEffect(() => {
+        dispatch(genreAction.getAll()).then(({payload}) => setGenres(payload.genres))
+    }, [])
 
     return (
         <div className={currentTheme === 'dark' ? css.card : css.lightCard}>
