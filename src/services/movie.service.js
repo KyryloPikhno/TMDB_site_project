f16b33c52f952bcf70c862a3378c90b75f68d0d2
KyryloPikhno <Mr.Good@i.ua>
2022-12-03 14:36:21 +0200

    added params

--- src/services/movie.service.js
+++ src/services/movie.service.js
@@ -1,13 +1,10 @@
 import {axiosService} from "./axios.service";
+
 import {baseURl, urls} from "../configs";
 
 
 const movieService = {
-    getAll: (genreName='') => axiosService.get(`${baseURl}${urls.movies}`, {
-        params: {
-            with_genres: genreName
-        }
-    })
+    getAll: (genreName='') => axiosService.get(`${baseURl}${urls.movies}`, {params: {with_genres: genreName}})
 };
 
 export {movieService};
