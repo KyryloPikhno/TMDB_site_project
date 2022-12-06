e4e7d8b9e2f2bc684c48b7fd6e30d90f78411c72
KyryloPikhno <Mr.Good@i.ua>
2022-12-06 21:27:55 +0200

    added carousel

--- src/components/MoviesCarousel/MoviesCarousel.js
+++ src/components/MoviesCarousel/MoviesCarousel.js
@@ -1,10 +1,7 @@
 import {useDispatch, useSelector} from "react-redux";
-import {useEffect, useRef} from "react";
+import {useEffect} from "react";
 import {movieActions} from "../../redux/slices/movie.slice";
-import Slider from "react-slick";
 
-
-import {Movie} from "../Movie/Movie";
 
 
 
@@ -14,35 +11,20 @@ const MoviesCarousel = () => {
     console.log(popularMovies);
 
     const dispatch = useDispatch()
-    const slider = useRef();
 
     useEffect(() => {
         dispatch(movieActions.getPopularMovies())
     }, [dispatch])
 
-    const settings = {
-        dots: true,
-        infinite: false,
-        speed: 500,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        responsive: [
-          {
-            breakpoint: 1024,
-            settings: {
-              slidesToShow: 3,
-              slidesToScroll: 3,
-              infinite: true,
-              dots: true
-            }
-          },
-        ]
-    };
+
 
     return (
-        <Slider ref={slider} {...settings}>
-            {popularMovies.results &&  popularMovies.results.map(movie => <Movie key={movie.id} movie={movie}/>)}
-        </Slider>
+
+
+                 <div>
+
+                 </div>
+
     );
 };
 
