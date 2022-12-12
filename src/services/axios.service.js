864deafc7aad9cbbd7a27b177093e2cf39addb5f
KyryloPikhno <Mr.Good@i.ua>
2022-12-12 19:51:41 +0200

    added pagination

--- src/services/axios.service.js
+++ src/services/axios.service.js
@@ -6,7 +6,7 @@ import {baseURl} from "../configs";
 const _accessTokenKey = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkMDZmNTMyOGVhYjMwN2I3MTM5MjNmZTUzYzU5MjU2OSIsInN1YiI6IjYzNGFiZTM0MTA4OWJhMDA3ZDUzODY4MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ACz4f6I94aAvceySLFzh9mH4Asy4rutGDbrOl1qbDtE';
 
 const axiosService = axios.create({
-    baseUrl:baseURl
+     baseURl: baseURl
 });
 
 axiosService.interceptors.request.use((config) => {
